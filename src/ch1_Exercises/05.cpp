33364b223b96d2eda05aa4b3a81e761fdd8d3006
kevin <kevin.d.hende@gmail.com>
2022-01-16 03:20:33 -0600

    05.cpp is also 06.cpp, 07.cpp is a continuation of the exercises

--- src/ch1_Exercises/05.cpp
+++ src/ch1_Exercises/05.cpp
@@ -1,13 +1,32 @@
 //
 // Created by kevin on 1/16/2022.
 //
-
+// this program also counts as 06.cpp
+// it's just a modification of the 05.cpp, so they are together in the same file.
 #include <iostream>
 
 int main() {
 
+    const int SENTINAL = 0;
+    int number = 0, largest = 0, second_largest = 0;
 
+    std::cout << "This program finds the largest integer in a list.\n"
+              << "Enter 0 to signal the end of the list.\n";
 
+    while(true) {
+        std::cout << " ? ";
+        std::cin >> number;
+
+        if(number == SENTINAL)
+            break;
+        else if(number > largest)
+            largest = number;
+        else if(second_largest < largest)
+            second_largest = number;
+    }
+
+    std::cout << "The largest value is " << largest << std::endl;
+    std::cout << "The second largest value is " << second_largest << std::endl;
 
     return 0;
 }
---
+++ src/ch1_Exercises/07.cpp
@@ -0,0 +1,16 @@
+//
+// Created by kevin on 1/16/2022.
+//
+// this program is a modification of a program from the text "AddIntegerList.cpp"
+// Write a program called "AverageList.cpp" -- "07.cpp" in our case --
+// that reads in a list fo integers representing exam scores and then prints out the average of them all.
+// Because some unprepared student may get a 0, the SENTINAL should be -1 to mark the end of the input.
+
+#include <iostream>
+
+int main() {
+
+
+
+    return 0;
+}
