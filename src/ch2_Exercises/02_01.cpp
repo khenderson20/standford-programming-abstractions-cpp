f054630ee490540bb844eebd079aac31ca46f538
kevin <kevin.d.hende@gmail.com>
2022-01-19 05:40:03 -0600

    Stanford C++ Library added to project

--- src/ch2_Exercises/02_01.cpp
+++ src/ch2_Exercises/02_01.cpp
@@ -1,7 +1,11 @@
+//
+// Created by kevin on 1/18/2022.
+//
 #include <iostream>
 #include "random.h"
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
     std::cout << "Stanford C++ Library is finally working!!!" << std::endl;
+
     return 0;
 }
